bed9f4134d7100a50ef036b0e939a09951c578d0
shlegelal <shlegel.alink0@gmail.com>
2023-03-19 10:37:34 +0300

    task 2

--- project/bfs.py
+++ project/bfs.py
@@ -5,11 +5,11 @@ def bfs(adj: gb.Matrix, start: int) -> list[int]:
     """
     Implementation of BFS algorithm for given directed graph and start vertex
     :param adj: Bool adjacency matrix of the graph
-    :param start: Index of starting vertex in adjacency matrix
+    :param start: Index of start vertex in adjacency matrix
     :return: List with counts of steps from start vertex to others.
         If vertex is not reachable, value in list equals -1.
     """
-    _is_correct_matrix(adj, start)
+    _is_correct_matrix(adj, [start])
 
     steps = gb.Vector.sparse(gb.INT64, size=adj.nrows)
     front = gb.Vector.sparse(gb.BOOL, size=adj.nrows)
@@ -26,7 +26,39 @@ def bfs(adj: gb.Matrix, start: int) -> list[int]:
     return [steps.get(i, default=-1) for i in range(steps.size)]
 
 
-def _is_correct_matrix(adj: gb.Matrix, start: int):
+def msbfs(adj: gb.Matrix, starts: list[int]) -> list[tuple[int, list[int]]]:
+    """
+    Implementation of Multi-source BFS algorithm for given directed graph and start vertexes
+    :param adj: Bool adjacency matrix of the graph
+    :param starts: Indexes of start vertexes in adjacency matrix
+    :return: list of start and parents where for each start vertex there is a list
+    of parent vertices for the corresponding vertex. If there are several possible
+    parent vertices, the one with the smaller index will be picked. The start vertices
+    will have -1 in these lists and unreachable vertices will have -2.
+    """
+    _is_correct_matrix(adj, starts)
+
+    parents = gb.Matrix.sparse(gb.INT64, nrows=len(starts), ncols=adj.ncols)
+    front = gb.Matrix.sparse(gb.INT64, nrows=len(starts), ncols=adj.ncols)
+    for row, start in enumerate(starts):
+        front[row, start] = -1
+
+    while front.nvals != 0:
+        parents.assign(front, mask=front.S)
+        front.apply(gb.INT64.POSITIONJ, out=front)
+        front.mxm(
+            adj,
+            semiring=gb.INT64.MIN_FIRST,
+            out=front,
+            mask=parents,
+            desc=gb.descriptor.RSC,
+        )
+
+    parents.assign_scalar(-2, mask=parents, desc=gb.descriptor.S & gb.descriptor.C)
+    return [(start, list(parents[i, :].vals)) for i, start in enumerate(starts)]
+
+
+def _is_correct_matrix(adj: gb.Matrix, starts: list[int]):
     """
     Check that graph matrix is square, matrix has boolean type,
     start vertices are not negative and not more the number of vertices
@@ -35,5 +67,6 @@ def _is_correct_matrix(adj: gb.Matrix, start: int):
         raise ValueError("Adjacency matrix must be square")
     if adj.type != gb.BOOL:
         raise ValueError(f"Matrix type {adj.type}, expected {gb.BOOL}")
-    if start < 0 or start >= adj.nrows:
-        raise ValueError(f"Start vertex is {start}, expected 0..{adj.nrows}")
+    for start in starts:
+        if start < 0 or start >= adj.nrows:
+            raise ValueError(f"Start vertex is {start}, expected 0..{adj.nrows}")
--- tests/test_bfs.py
+++ tests/test_bfs.py
@@ -2,7 +2,7 @@ import pytest
 import utils
 import pygraphblas as gb
 
-from project.bfs import bfs
+from project.bfs import bfs, msbfs
 
 
 @pytest.mark.parametrize(
@@ -19,16 +19,35 @@ def test_bfs_algorithm(adj: gb.Matrix, start: int, expected: list[int]):
 
 
 @pytest.mark.parametrize(
+    "adj, starts, expected",
+    map(
+        lambda r: (
+            utils.adj_from_res(r["adj"]),
+            r["starts"],
+            [tuple(pair_list) for pair_list in r["expected"]],
+        ),
+        utils.load_res("test_msbfs"),
+    ),
+    ids=utils.get_title("test_msbfs"),
+)
+def test_msbfs(adj: gb.Matrix, starts: list[int], expected: list[int]):
+    actual = msbfs(adj, starts)
+
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
     "adj, start",
     map(
         lambda r: (
             utils.adj_from_res(r["adj"], gb.types.BOOL if r["type"] else gb.INT64),
             r["start"],
         ),
-        utils.load_res("test_bfs_input"),
+        utils.load_res("test_input"),
     ),
-    ids=utils.get_title("test_bfs_input"),
+    ids=utils.get_title("test_input"),
 )
-def test_bfs_input(adj: gb.Matrix, start: int):
+def test_input(adj: gb.Matrix, start: int):
     with pytest.raises(ValueError):
         bfs(adj, start)
+        msbfs(adj, [start])
